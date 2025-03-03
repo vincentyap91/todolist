e447404bb8595b5d08701a17d89cfe3447b5a896
Vincent Yap <vincenthuei91@gmail.com>
2025-03-03 15:15:28 +0800

    undo

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
   API_URL: import.meta.env.PROD 
-    ? 'http://vincenthuei91.infinityfreeapp.com/api'
+    ? 'https://vincenthuei91.infinityfreeapp.com/api'
     : 'http://localhost:5000',
   GITHUB_PAGES_URL: 'https://vincentyap91.github.io',
   APP_PATH: '/todolist'
